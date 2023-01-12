f957f5495404579a84f127492509e088b9965d96
Shirajuki <shirajuki00@gmail.com>
2023-01-12 19:44:03 +0100

    feat: add partials support through vite-plugin-handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+import { resolve } from 'path'
+import handlebars from 'vite-plugin-handlebars'
+
 export default {
-	root: "src",
-	build: {
-		outDir: "../dist",
-		emptyOutDir: true,
-	},
-};
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve('src', 'index.html'),
+        canvas: resolve('src', 'canvas.html'),
+        dom: resolve('src', 'dom.html'),
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve('src', 'partials'),
+    }),
+  ],
+}
