3f55b67404f50d02c41c277dbaa013ffbea28e85
Shirajuki <shirajuki00@gmail.com>
2023-01-12 16:15:37 +0100

    feat: cleanup the project and code

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -15,42 +15,41 @@ class CanvasEngine extends Engine {
 		const particles = new Array(this.count);
 		const rnd = [1, -1];
 		for (let i = 0; i < this.count; i++) {
-			const size = 10 + Math.random() * 100;
+			const size = 10 + Math.random() * 80;
 			const x = Math.random() * this.width;
 			const y = Math.random() * (this.height - size);
-			// const dx = 2 * Math.random() * rnd[Math.floor(Math.random() * 2)];
-			// const dy = 2 * Math.random() * rnd[Math.floor(Math.random() * 2)];
-			const [dx, dy] = [2 + Math.random(), 2 + Math.random()];
+			const [dx, dy] = [
+				3 * Math.random() * rnd[Math.floor(Math.random() * 2)],
+				3 * Math.random() * rnd[Math.floor(Math.random() * 2)],
+			];
 			particles[i] = { x, y, size: size, dx, dy };
 		}
 		this.particles = particles;
 
 		this.ctx = this.canvas.getContext("2d");
-		this.ctx.fillStyle = "black";
 		this.ctx.strokeStyle = "white";
 		this.ctx.lineWidth = 1;
-		this.ctx.webkitImageSmoothingEnabled = false;
-		this.ctx.imageSmoothingEnabled = false;
 	}
 	render() {
+		// Clear the canvas
 		this.ctx.clearRect(0, 0, this.width, this.height);
+
+		// Particle animation
 		const particles = this.particles;
 		for (let i = 0; i < this.count; i++) {
 			const r = particles[i];
 			r.x -= r.dx;
 			r.y -= r.dy;
-			if (r.x + r.size < 0) r.x = this.width + r.size;
-			else if (r.y + r.size < 0) r.y = this.height + r.size;
-			// if (r.x + r.size < 0) r.dx *= -1;
-			// else if (r.y + r.size < 0) r.dy *= -1;
-			// if (r.x > this.width) r.dx *= -1;
-			// else if (r.y > this.height) r.dy *= -1;
+			if (r.x + r.size < 0) r.dx *= -1;
+			else if (r.y + r.size < 0) r.dy *= -1;
+			if (r.x > this.width) r.dx *= -1;
+			else if (r.y > this.height) r.dy *= -1;
 			this.ctx.beginPath();
 			this.ctx.arc(r.x, r.y, r.size, 0, 2 * Math.PI);
 			this.ctx.stroke();
 		}
-		this.fpsmeter.tick();
 
+		this.fpsmeter.tick();
 		this.request = window.requestAnimationFrame(() => this.render());
 	}
 }
--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,6 +1,6 @@
 import Engine from "./engine.js";
 
-class CanvasEngine extends Engine {
+class DOMEngine extends Engine {
 	constructor() {
 		super();
 	}
@@ -13,11 +13,15 @@ class CanvasEngine extends Engine {
 
 		// Particle creation
 		const particles = new Array(this.count);
+		const rnd = [1, -1];
 		for (let i = 0; i < this.count; i++) {
-			const size = 10 + Math.random() * 100;
+			const size = 10 + Math.random() * 80 * 2;
 			const x = Math.random() * this.width;
 			const y = Math.random() * (this.height - size);
-			const [dx, dy] = [2 + Math.random(), 2 + Math.random()];
+			const [dx, dy] = [
+				3 * Math.random() * rnd[Math.floor(Math.random() * 2)],
+				3 * Math.random() * rnd[Math.floor(Math.random() * 2)],
+			];
 
 			const particle = document.createElement("div");
 			particle.className = "particle";
@@ -31,22 +35,26 @@ class CanvasEngine extends Engine {
 		this.particles = particles;
 	}
 	render() {
+		// Particle animation
 		const particles = this.particles;
 		for (let i = 0; i < this.count; i++) {
 			const r = particles[i];
 			r.x -= r.dx;
 			r.y -= r.dy;
-			if (r.x + r.size < 0) r.x = this.width + r.size;
-			else if (r.y + r.size < 0) r.y = this.height + r.size;
+			if (r.x + r.size < 0) r.dx *= -1;
+			else if (r.y + r.size < 0) r.dy *= -1;
+			if (r.x > this.width) r.dx *= -1;
+			else if (r.y > this.height) r.dy *= -1;
 			r.el.style.left = r.x + "px";
 			r.el.style.top = r.y + "px";
 		}
+
 		this.fpsmeter.tick();
 		this.request = window.requestAnimationFrame(() => this.render());
 	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	const engine = new CanvasEngine();
+	const engine = new DOMEngine();
 	engine.render();
 });
